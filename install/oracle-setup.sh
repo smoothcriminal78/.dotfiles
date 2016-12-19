16bcf62bf35f51926de84914c7e012bab4f7e244
root <dim.zaycev@gmail.com>
2016-12-19 16:08:35 +0300

    fix

---
+++ install/oracle-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo -n "Specify a version(...,7,8) id to be installed:"
+read javaVer
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java"$javaVer"-installer
+echo oracle-java"$javaVer"-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections update-java-alternatives -s java-"$javaVer"-oracle
